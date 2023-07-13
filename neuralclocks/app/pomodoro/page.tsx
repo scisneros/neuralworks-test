abae190b6063869f0e3f1067bf4c26e19d24099d
scisneros <sebastian.cisneros384@gmail.com>
2023-07-13 19:26:28 -0400

    Base timer layout

---
+++ neuralclocks/app/pomodoro/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Pomodoro() {
+  const [stage, setStage] = useState(0);
+
+  const stages: Array<{
+    id: number;
+    label: string;
+    duration: number;
+  }> = [
+    {
+      id: 0,
+      label: "Pomodoro",
+      duration: 25 * 60,
+    },
+    {
+      id: 1,
+      label: "Short Break",
+      duration: 5 * 60,
+    },
+    {
+      id: 2,
+      label: "Long Break",
+      duration: 15 * 60,
+    },
+  ];
+
+  return (
+    <div className="text-center">
+      <h1>Pomodoro</h1>
+      {stages.map((step) => (
+        <button
+          className="mx-4 mt-4"
+          key={step.id}
+          onClick={() => setStage(step.id)}
+        >
+          {step.label}
+        </button>
+      ))}
+      <div className="mt-4 text-8xl">{stages[stage].duration}</div>
+      <button className="mx-4 mt-4">Start</button>
+      <button className="mx-4 mt-4">Pause</button>
+      <button className="mx-4 mt-4">Reset</button>
+    </div>
+  );
+}
