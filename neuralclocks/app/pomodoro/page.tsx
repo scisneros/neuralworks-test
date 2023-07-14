8106ef62300d8371a3b3bb5c6e1fed5e6419f96b
scisneros <sebastian.cisneros384@gmail.com>
2023-07-13 20:34:00 -0400

    Timer functionalities: Start, Pause, Reset

--- neuralclocks/app/pomodoro/page.tsx
+++ neuralclocks/app/pomodoro/page.tsx
@@ -1,10 +1,29 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { secToTime } from "../../utils/utils";
+import clsx from "clsx";
 
 export default function Pomodoro() {
   const [stage, setStage] = useState(0);
+  const [time, setTime] = useState(0); // in seconds
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    if (isRunning) {
+      const interval = setInterval(() => {
+        setTime((time) => time - 1);
+      }, 1000);
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [isRunning]);
+
+  useEffect(() => {
+    setIsRunning(false);
+    setTime(stages[stage].duration);
+  }, [stage]);
 
   const stages: Array<{
     id: number;
@@ -40,10 +59,28 @@ export default function Pomodoro() {
           {step.label}
         </button>
       ))}
-      <div className="mt-4 text-8xl">{secToTime(stages[stage].duration)}</div>
-      <button className="mx-4 mt-4">Start</button>
-      <button className="mx-4 mt-4">Pause</button>
-      <button className="mx-4 mt-4">Reset</button>
+      <div className="mt-4 text-8xl">{secToTime(time)}</div>
+      <button
+        className={clsx("mx-4 mt-4", { underline: isRunning })}
+        onClick={() => setIsRunning(true)}
+      >
+        Start
+      </button>
+      <button
+        className={clsx("mx-4 mt-4", { underline: !isRunning })}
+        onClick={() => setIsRunning(false)}
+      >
+        Pause
+      </button>
+      <button
+        className="mx-4 mt-4"
+        onClick={() => {
+          setTime(stages[stage].duration);
+          setIsRunning(false);
+        }}
+      >
+        Reset
+      </button>
     </div>
   );
 }
